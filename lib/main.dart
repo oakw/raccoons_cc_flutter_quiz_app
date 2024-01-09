b20c5adfe3178133f3e5536747a7674becf8a4dc
Mārtiņš P <mrcukurs73@gmail.com>
2024-01-09 20:58:44 +0200

    Added answer options

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,25 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.only(top: 30, left: 10),
-          child: Text("Kurš ir ātrākais sauszemes dzīvnieks?"),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 10),
+              child: Text("Kurš ir ātrākais sauszemes dzīvnieks?"),
+            ),
+            Row(
+              children: [
+                Text("Bruņurupucis"),
+                Text("Jaguārs"),
+              ],
+            ),
+            Row(
+              children: [
+                Text("Lauva"),
+                Text("Strauss"),
+              ],
+            )
+          ],
         ),
       ),
     );
