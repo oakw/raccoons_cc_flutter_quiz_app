6e30e0712a96fd71763ab7ab2ef7134baf545543
Mārtiņš P <mrcukurs73@gmail.com>
2024-01-21 17:49:54 +0200

    More questions added (advanced)

--- lib/main.dart
+++ lib/main.dart
@@ -13,31 +13,32 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 30, left: 10),
-              child: Text("Kurš ir ātrākais sauszemes dzīvnieks?"),
+            Question(
+              text: "Kurš ir ātrākais sauszemes dzīvnieks?",
+              options: [
+                Option(text: "Bruņurupucis", isCorrect: false),
+                Option(text: "Jaguārs", isCorrect: true),
+                Option(text: "Lauva", isCorrect: false),
+                Option(text: "Strauss", isCorrect: false),
+              ],
             ),
-            // Pirmā rinda ar atbilžu variantiem
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Option(text: "Bruņurupucis", isCorrect: false),
-                  Option(text: "Jaguārs", isCorrect: true),
-                ],
-              ),
+            Question(
+              text: "Kā sauc sīku ūdens pilienu uzkrāšanos atmosfēras zemākajos slāņos?",
+              options: [
+                Option(text: "Iztvaikošana", isCorrect: false),
+                Option(text: "Varavīksne", isCorrect: false),
+                Option(text: "Rasa", isCorrect: false),
+                Option(text: "Migla", isCorrect: true),
+              ],
             ),
-            // Otrā rinda ar atbilžu variantiem
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Option(text: "Lauva", isCorrect: false),
-                  Option(text: "Strauss", isCorrect: false),
-                ],
-              ),
+            Question(
+              text: "Kurš ieguva FIFA 2023. gada labākā futbolista balvu?",
+              options: [
+                Option(text: "Krištianu Ronaldu", isCorrect: false),
+                Option(text: "Lionels Mesi", isCorrect: true),
+                Option(text: "Roberts Levandovskis", isCorrect: false),
+                Option(text: "Karīms Benzemā", isCorrect: false),
+              ],
             ),
           ],
         ),
@@ -79,7 +80,65 @@ class Option extends StatelessWidget {
       },
       child: Text(
         text,
+        textAlign: TextAlign.center,
         style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
+
+// Jautājums ar tā tekstu un atbilžu variantiem
+class Question extends StatelessWidget {
+  final String text;
+  final List<Option> options;
+  const Question({
+    super.key,
+    required this.text,
+    required this.options,
+  });
+
+  // Sagatavo jautājuma atbilžu rindas
+  List<Widget> getOptionRows() {
+    List<Widget> rows = List.empty(growable: true);
+    List<Option> rowOptions = List.empty(growable: true);
+
+    for (var i = 0; i < options.length; i++) {
+      rowOptions.add(options[i]);
+
+      // Vienā rindā ir divi elementi vai viens, ja vairāk atbilžu iespēju nav
+      if (rowOptions.length == 2 || i + 1 == options.length) {
+        rows.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: rowOptions.toList(),
+            ),
+          ),
+        );
+
+        rowOptions.clear();
+      }
+    }
+
+    return rows;
+  }
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ...getOptionRows()
+        ],
       ),
     );
   }
